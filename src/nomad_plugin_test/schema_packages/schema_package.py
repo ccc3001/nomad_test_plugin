ad0bd5d7f343a8a7c6b137d50b6fde8ce0ab33d3
ccc3001 <69085353+ccc3001@users.noreply.github.com>
2025-05-08 14:44:30 +0200

    Update schema_package.py

--- src/nomad_plugin_test/schema_packages/schema_package.py
+++ src/nomad_plugin_test/schema_packages/schema_package.py
@@ -7,28 +7,33 @@ if TYPE_CHECKING:
 from nomad.config import config
 from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import Quantity, SchemaPackage
+from nomad.metainfo import Quantity, Package
 
+# Load plugin configuration from pyproject.toml entry point
 configuration = config.get_plugin_entry_point(
     'nomad_plugin_test.schema_packages:schema_package_entry_point'
 )
 
-m_package = SchemaPackage()
+# Define the metainfo package
+m_package = Package(name='new_schema_package')
 
 
+# Define your schema class
 class NewSchemaPackage(Schema):
+    m_def = m_package.m_schema()  # Register class with the package
+
     name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
     )
+
     message = Quantity(type=str)
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         super().normalize(archive, logger)
-        logger.info('NewSchema.normalize', parameter=configuration.parameter)
+        logger.info('NewSchemaPackage.normalize', parameter=configuration.parameter)
+
         if self.name:
             self.message = f'Hello {self.name}!'
         else:
             self.message = "Hello, unnamed package!"
-
-
-m_package.__init_metainfo__()
