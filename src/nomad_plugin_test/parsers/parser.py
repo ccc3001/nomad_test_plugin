7c0232b15993601c122586b08a8a9300905fce92
ccc3001 <69085353+ccc3001@users.noreply.github.com>
2025-05-08 13:33:29 +0200

    Update parser.py

--- src/nomad_plugin_test/parsers/parser.py
+++ src/nomad_plugin_test/parsers/parser.py
@@ -1,18 +1,13 @@
-from typing import (
-    TYPE_CHECKING,
-)
-
-if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
-        BoundLogger,
-    )
+from typing import TYPE_CHECKING
 
 from nomad.config import config
-#from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import EntryArchive
+    from structlog.stdlib import BoundLogger
+
+# from nomad.datamodel.metainfo.workflow import Workflow
 
 configuration = config.get_plugin_entry_point(
     'nomad_plugin_test.parsers:parser_entry_point'
@@ -20,7 +15,6 @@ configuration = config.get_plugin_entry_point(
 
 
 class NewParser(MatchingParser):
-   
     def parse(
         self,
         mainfile: str,
@@ -32,4 +26,4 @@ class NewParser(MatchingParser):
         archive.data.comment = "test"
         logger.info('NewParser.parse', parameter=configuration.parameter)
         logger.warn("some warning")
-        #archive.workflow2 = Workflow(name='test')
+        # archive.workflow2 = Workflow(name='test')
